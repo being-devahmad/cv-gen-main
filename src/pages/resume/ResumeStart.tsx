afb267fdaf8a0672c0537fe1a93e617d3717e581
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-12-08 21:59:35 +0500

    updated

--- src/pages/resume/ResumeStart.tsx
+++ src/pages/resume/ResumeStart.tsx
@@ -1,9 +1,40 @@
+// import ResumeForm2 from '@/components/forms/ResumeForm/ResumeForm2';
+// // import { PreviewSection } from '@/components/resumeDashboard/ResumePreview';
+// // import { PreviewSection } from '@/components/resumeDashboard/ResumePreview';
+// import { ResumeProvider } from '@/hooks/useResume';
+// // import Chikorita from '@/templates/chikorita';
+// import { Azurill } from '@/templates/azurill';
+// // import { Chikorita } from '@/templates/chikorita';
+// import React from 'react';
+
+// const ResumeStart: React.FC = () => {
+//     return (
+//         <ResumeProvider>
+//             <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
+//                 <div className="w-full lg:w-1/2 overflow-y-auto border-b lg:border-b-0 lg:border-r border-gray-200">
+//                     <ResumeForm2 />
+//                 </div>
+//                 <div className="w-full lg:w-1/2 overflow-y-auto">
+//                     {/* <PreviewSection /> */}
+//                     <Azurill />
+//                     {/* <Chikorita /> */}
+//                 </div>
+//             </div>
+//         </ResumeProvider>
+//     );
+// };
+
+// export default ResumeStart;
+
+
+
 import ResumeForm2 from '@/components/forms/ResumeForm/ResumeForm2';
-import { PreviewSection } from '@/components/resumeDashboard/ResumePreview';
+// import { PreviewSection } from '@/components/resumeDashboard/ResumePreview';
 // import { PreviewSection } from '@/components/resumeDashboard/ResumePreview';
 import { ResumeProvider } from '@/hooks/useResume';
-// import Chikorita from '@/templates/chikorita';
-import { Azurill } from '@/templates/azurill';
+// import Azurill from '@/templates/azurill';
+import Chikorita from '@/templates/chikorita';
+// import { Azurill } from '@/templates/azurill';
 // import { Chikorita } from '@/templates/chikorita';
 import React from 'react';
 
@@ -12,12 +43,12 @@ const ResumeStart: React.FC = () => {
         <ResumeProvider>
             <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
                 <div className="w-full lg:w-1/2 overflow-y-auto border-b lg:border-b-0 lg:border-r border-gray-200">
-                    <ResumeForm2/>
+                    <ResumeForm2 />
                 </div>
                 <div className="w-full lg:w-1/2 overflow-y-auto">
                     {/* <PreviewSection /> */}
-                    <Azurill />
-                    {/* <Chikorita /> */}
+                    {/* <Azurill /> */}
+                    <Chikorita />
                 </div>
             </div>
         </ResumeProvider>
@@ -25,4 +56,3 @@ const ResumeStart: React.FC = () => {
 };
 
 export default ResumeStart;
-
